f3274908309c8c498363cc8f872c7b55524f3c71
conrad96 <conrad.mugisha@gmail.com>
2019-04-20 15:04:06 +0300

    login page code

---
+++ lib/screens/welcome.dart
@@ -0,0 +1,42 @@
+import '../main.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'login.dart';
+
+class Welcome extends StatelessWidget
+{
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      body: Center(
+        child: new SplashScreen(
+          seconds: 20,
+          navigateAfterSeconds: new AfterSplash(),
+          //title: new Text('Welcome'),
+          image: new Image.asset('assets/images/my_company.png'),
+          backgroundColor: Colors.redAccent.shade200,
+          styleTextUnderTheLoader: new TextStyle(),
+          photoSize: 200.0,
+          loaderColor: Colors.white
+        ),
+      ),
+    );
+  }
+}
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+      title: Center(
+        child:new Text("Portal") ,
+      ),
+      automaticallyImplyLeading: false
+      ),
+      body: new Center(
+        child: LoginScreen(),
+      ),
+    );
+  }
+}
